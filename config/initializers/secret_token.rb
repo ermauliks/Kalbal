0a03662c2cc24d77678ec3c74da3a9784e7a8100
mauliks <mauliks@thoughtworks.com>
2013-10-07 15:41:24 +0530

    Project setup, adding grunt, bower and yeoman

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kalbal::Application.config.secret_key_base = '1dd6e14a25e87e32a53c04acd96e65082d1d5285409c7c480a9eac0d5e4c258d8e0f8420b84c9bb44fc8af723569ba20d7adcb885dbca02a939ce1da027608ce'
---
+++ ngapp/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('kalbalApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
